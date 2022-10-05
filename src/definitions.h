fd2ad5b3d905290354b5b072871821678fef732d
Daniel Reyes <70819533+ZeroSkyer@users.noreply.github.com>
2022-10-04 20:34:21 -0500

    Update definitions.h
    Updating engine version

--- src/definitions.h
+++ src/definitions.h
@@ -21,7 +21,7 @@
 #define FS_DEFINITIONS_H_877452FEC245450C9F96B8FD268D8963
 
 static constexpr auto STATUS_SERVER_NAME = "Thornia Engine";
-static constexpr auto STATUS_SERVER_VERSION = "1.3";
+static constexpr auto STATUS_SERVER_VERSION = "1.4";
 static constexpr auto STATUS_SERVER_DEVELOPERS = "Thornia Team, Mark Samman and The Forgotten Server Developers";
 
 static constexpr auto CLIENT_VERSION_MIN = 1200;
